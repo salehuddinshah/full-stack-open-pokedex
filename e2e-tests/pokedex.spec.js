a12a595017476a502f5334b074cb282c3b2a9e7e
salehuddinshah <udin5278@gmail.com>
2026-02-04 12:04:05 +0800

    init e2e test

--- e2e-tests/pokedex.spec.js
+++ e2e-tests/pokedex.spec.js
@@ -1,9 +1,18 @@
 const { test, describe, expect } = require('@playwright/test')
 
+const baseURL = 'http://localhost:8080'
+
 describe('Pokedex', () => {
   test('front page can be opened', async ({ page }) => {
-    await page.goto('http://localhost:8080')
+    await page.goto(baseURL)
     await expect(page.getByText('ivysaur')).toBeVisible()
     await expect(page.getByText('Pokémon and Pokémon character names are trademarks of Nintendo.')).toBeVisible()
+  })
+
+  test('pokemon page can be navigated to', async ({ page }) => {
+    await page.goto(baseURL)
+    await page.locator(page.getByText('ivysaur')).click()
+    await expect(page).toHaveURL(`${baseURL}/pokemon/ivysaur`)
+    await expect(page.getByText(/chlorophyll/i)).toBeVisible()
   })
 })
