768b4b9b49d96c1fb9c9ec780e4a7b475b6aa38d
salehuddinshah <udin5278@gmail.com>
2026-02-04 13:58:16 +0800

    prepare for automatic deploy

--- app.js
+++ app.js
@@ -2,9 +2,17 @@ import express from 'express'
 const app = express()
 
 // get the port from env variable
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 3001
 
 app.use(express.static('dist'))
+
+app.get('/version', (req, res) => {
+  res.send('1')
+})
+
+app.get('/health', (req, res) => {
+  res.status(200).send('ok')
+})
 
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
